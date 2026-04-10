a3d820ae1979df321b60f6ad4f21377605951926
Wassn Al Nabhan <wassn.hashim@gmail.com>
2026-04-09 20:48:58 -0400

    added code to product list

--- A2_IOS_Wassn_101468092/ProductListView.swift
+++ A2_IOS_Wassn_101468092/ProductListView.swift
@@ -1,7 +1,26 @@
-//
-//  ProductListView.swift
-//  A2_IOS_Wassn_101468092
-//
-//  Created by user947626 on 4/9/26.
-//
+import SwiftUI
+import CoreData
 
+struct ProductListView: View {
+    
+    @FetchRequest(
+        entity: Product.entity(),
+        sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)]
+    ) var products: FetchedResults<Product>
+    
+    var body: some View {
+        List {
+            ForEach(products, id: \.objectID) { product in
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(product.name ?? "No Name")
+                        .font(.headline)
+                    
+                    Text(product.productDescription ?? "No Description")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
+        .navigationTitle("All Products")
+    }
+}
